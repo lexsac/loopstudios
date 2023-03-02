26a99a9c4635dc39271a7f0d40c3bf473e7176eb
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-01 17:58:34 -0800

    Created NavLinks components

---
+++ src/components/NavLinks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavLinks = () => {
+    return (
+        <ul>
+            <li>About</li>
+            <li>Careers</li>
+            <li>Events</li>
+            <li>Product</li>
+            <li>Support</li>
+        </ul>
+    )
+}
+
+export default NavLinks;
