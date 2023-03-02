8f5456d118fdbc89b18096c4d8898184a5641f9c
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-01 18:13:14 -0800

    Translated <li> elements into SocialLink components

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import SocialLink from './SocialLink';
+import FacebookIcon from '../images/icon-facebook.svg';
+import TwitterIcon from '../images/icon-twitter.svg';
+import PinterestIcon from '../images/icon-pinterest.svg';
+import InstagramIcon from '../images/icon-instagram.svg';
 
 const SocialLinks = () => {
     return (
-        <>
-            <p>Social Links</p>
-        </>
+        <ul>
+            <SocialLink src={FacebookIcon} alt={'Facebook icon'} />
+            <SocialLink src={TwitterIcon} alt={'Twitter icon'} />
+            <SocialLink src={InstagramIcon} alt={'Instagram icon'} />
+            <SocialLink src={PinterestIcon} alt={'Pinterest icon'} />
+        </ul>
     )
 }
 
