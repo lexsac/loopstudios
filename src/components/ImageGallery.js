a80c26235c9b5c5f6a0183e69b2e29c943cc89e6
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-01 18:16:52 -0800

    Added images gallery photo import paths

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Image from './Image';
+import Image from './ResponsiveImage';
+import HeadingText from './HeadingText';
+import Button from'./Button';
+import curiosityImageMobile from '../images/mobile/image-curiosity.jpg';
+import curiosityImageDesktop from '../images/desktop/image-curiosity.jpg';
+import deepearthImageMobile from '../images/mobile/image-deep-earth.jpg';
+import deepearthImageDesktop from '../images/desktop/image-deep-earth.jpg';
+import fisheyeMobile from '../images/mobile/image-fisheye.jpg';
+import fisheyeDesktop from '../images/desktop/image-fisheye.jpg';
+import fromaboveImageMobile from '../images/mobile/image-from-above.jpg';
+import fromaboveImageDesktop from '../images/desktop/image-from-above.jpg';
+import gridImageMobile from '../images/mobile/image-grid.jpg';
+import gridImageDesktop from '../images/desktop/image-grid.jpg';
+import nightarcadeImageMobile from '../images/mobile/image-night-arcade.jpg';
+import nightarcadeImageDesktop from '../images/desktop/image-night-arcade.jpg';
+import pocketborealisImageMobile from '../images/mobile/image-pocket-borealis.jpg';
+import pocketborealisImageDesktop from '../images/desktop/image-pocket-borealis.jpg';
+import soccerteamImageMobile from '../images/mobile/image-soccer-team.jpg';
+import soccerteamImageDesktop from '../images/desktop/image-soccer-team.jpg';
 
 const ImageGallery = () => {
     return (
-        <>
-            <Image />
-            <Image />
-            <Image />
-        </>
-    )
-}
+      <>
+        <HeadingText text={'Our creations'}/>
+        <Button text={'See all'}/>
+        <p>This is where the images go</p>
+      </>
+    );
+};
 
 export default ImageGallery;
