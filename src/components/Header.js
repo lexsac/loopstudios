f1e88c581461108b4503ef6d6322da7a5a6c387d
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-01 18:09:17 -0800

    Added MobileNavButton to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from './Logo';
 import Navigation from './Navigation';
+import MobileNavButton from './MobileNavButton';
 
 const Header = () => {
     return (
         <>
             <Logo />
+            <MobileNavButton />
             <Navigation />
         </>
     )
