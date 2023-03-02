7a55ecf00a25b1f81720eae9f645f0683842fdd2
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-02 13:43:09 -0800

    Added div wrapper with class='hero'

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,13 +3,14 @@ import HeroImage from './HeroImage';
 import HeroText from './HeroText';
 import heroImageMobile from '../images/mobile/image-hero.jpg';
 import heroImageDesktop from '../images/desktop/image-hero.jpg';
+import './index.css';
 
 const Hero = () => {
     return (
-        <>
+        <div className="hero">
             <HeroImage mobileImage={heroImageMobile} desktopImage={heroImageDesktop} description = "hero-image" />
             <HeroText text={'Immersive experiences that deliver'}/> 
-        </>
+        </div>
     )
 }
 
