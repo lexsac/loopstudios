c9d4ec67bfbda67307653465001cbf3c473c3e79
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-01 18:15:32 -0800

    Changed HeaderImage --> HeroImage naming

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Header from './components/Header';
-import HeaderImage from './components/HeaderImage';
+import HeroImage from './components/HeroImage';
 import InfoSection from './components/InfoSection';
 import ImageGallery from './components/ImageGallery';
 import Footer from './components/Footer';
+import heroImageMobile from './images/mobile/image-hero.jpg';
+import heroImageDesktop from './images/desktop/image-hero.jpg';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HeaderImage />
+      <HeroImage mobileImage={heroImageMobile} desktopImage={heroImageDesktop} description = "hero-image" />
       <InfoSection />
       <ImageGallery />
       <Footer />
