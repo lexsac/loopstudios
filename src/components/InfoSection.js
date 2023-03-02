905806dcca1c260695a60a53dc080324029605e9
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-02 13:44:14 -0800

    Added div wrapper and classNames

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -3,18 +3,18 @@ import Image from './ResponsiveImage';
 import HeadingText from './HeadingText';
 import interactiveImageMobile from '../images/mobile/image-interactive.jpg';
 import interactiveImageDesktop from '../images/desktop/image-interactive.jpg';
-import '../styles/InfoSection.css';
+import './index.css';
 
 const InfoSection = () => {
     return (
-        <>
+        <div className="info-section">
             <Image mobileImage={interactiveImageMobile} desktopImage={interactiveImageDesktop} description={'Man wearing a VR headset'} />
             <HeadingText text={'The leader in interactive VR'} />
-            <p>Founded in 2011, Loopstudios has been producing world-class virtual 
+            <p className="info-section__description">Founded in 2011, Loopstudios has been producing world-class virtual 
                 reality projects for some of the best companies around the globe. 
                 Our award-winning creations have transformed businesses through digital 
                 experiences that bind to their brand.</p>
-        </>
+        </div>
     )
 }
 
